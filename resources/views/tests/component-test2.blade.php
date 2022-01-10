8bdd0fa2c64aaecab2bf998cb66c90e8941b9058
nanakahorii7 <t1dxoxo@icloud.com>
2022-01-10 13:15:30 +0900

    クラスベースコンポーネントの設定

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,4 +1,7 @@
 <x-tests.app>
 <x-slot name="header">ヘッダ-2</x-slot>
     コンポーネントテスト2
+    <x-test-class-base classBaseMessage="メッセージです"/>
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から値を変更しています。"/>
 </x-tests.app>
